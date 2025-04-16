f27b2c3a2fd2538072544cea4eb9c37839920a2a
SergeyGriko <206880@gmail.com>
2025-04-16 04:39:53 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,32 @@ public class Main {
             }
         }
 // Третья задача
+        int year = 2021;
+        if (year >= 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+        } else {
+            System.out.println("В " + year + " году ещё не придумали високосный год");
+        }
+// Четвертая задача
+        int deliveryDistance = 95;
+        int day = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Доставка займет " + day + " сутки");
+        }
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            day = day + 1;
+            System.out.println("Доставка займет " + day + " суток");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            day = day + 2;
+            System.out.println("Доставка займет " + day + " суток");
+        } else {
+            System.out.println("Доставка свыше 100км не осуществялется");
+        }
+// Четвертая задача
 
     }
 }
